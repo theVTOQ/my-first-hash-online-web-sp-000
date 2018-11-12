72c8ccb727e7d231584c0931a7cc5ea96df6e4d4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-12 19:47:16 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -35,13 +35,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gunpowder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
